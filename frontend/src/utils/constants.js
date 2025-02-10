c371c60aa6ccc0854a9e007e0a116f118f50317e
Shubham Patil <shubhamspatilnbr@gmail.com>
2025-02-10 07:27:56 +0530

    update base url for deployment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "/api";
 
 export const DEFAULT_AVATAR =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1zSyUBd-rcAC5C4B1u4f0UzFHScTDX5u60bDIA1P-9mlRwOhOmN1eQnoLbyYg-LFXZE0&usqp=CAU";
